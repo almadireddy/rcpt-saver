55d00bbfd3149d596cc24b7a21dee5af9e136cd6
Al Madireddy <al.madireddy@gmail.com>
2019-01-26 20:19:52 -0600

    remove whitespace

--- server/image/image.route.js
+++ server/image/image.route.js
@@ -3,7 +3,7 @@ const path = require('path');
 const router = express.Router(); // eslint-disable-line new-cap
 
 router.route('/uploads/:p').get((req, res) => {
-  res.sendFile(path.join(__dirname, '../../uploads', req.params.p));
+  res.sendFile(path.join(__dirname, '../../uploads', req.params.p.trim()));
 })
 
 module.exports = router;
