19604e2a3e01d502fb099446456261afc990f0cc
Al Madireddy <al.madireddy@gmail.com>
2019-01-27 06:36:52 -0600

    increased filesize limit

--- server/receipt/receipt.route.js
+++ server/receipt/receipt.route.js
@@ -13,7 +13,7 @@ const storage = multer.diskStorage({
   }
 })
 
-const upload = multer({ storage: storage })
+const upload = multer({ storage: storage, limits: { fieldSize: 25 * 1024 * 1024 } })
 const router = express.Router(); // eslint-disable-line new-cap
 
 router.route('/')
