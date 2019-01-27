caee62b5d700a521dd5a3a12827813f0fb18bd4d
Al Madireddy <al.madireddy@gmail.com>
2019-01-27 02:26:35 -0600

    able to delete

--- server/receipt/receipt.controller.js
+++ server/receipt/receipt.controller.js
@@ -139,7 +139,7 @@ function list(req, res, next) {
  * @returns {Receipt}
  */
 function remove(req, res, next) {
-  Receipt.findByIdAndDelete(req.params.id)
+  Receipt.deleteOne({_id: req.params.id})
 }
 
 module.exports = { load, get, create, update, list, remove, ocr, categorize };
