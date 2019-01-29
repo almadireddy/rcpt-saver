41a98d3b176105299507a8508c6d2caeb6581fe0
Al Madireddy <al.madireddy@gmail.com>
2019-01-28 20:20:59 -0600

    hackathon in done bub

--- server/receipt/receipt.route.js
+++ server/receipt/receipt.route.js
@@ -26,7 +26,4 @@ router.route('/:id')
   .get(receiptController.get)
   .delete(receiptController.remove);
 
-/** Load user when API with userId route parameter is hit */
-router.param('userId', receiptController.load);
-
 module.exports = router;
